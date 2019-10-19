107627a67f78daf78b6642e9abf0df0d17e9b6d4
mugendinjue <mugendinjue7@gmail.com>
2019-10-19 23:03:32 +0300

    register all the models to the admin module

--- award/admin.py
+++ award/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Rating,Tag,Technology,Profile,Project
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Rating)
+admin.site.register(Project)
+admin.site.register(Technology)
+admin.site.register(Tag)
