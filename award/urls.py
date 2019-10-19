6de8d6b7c82e495b4636a6fdf13742adf2a167eb
mugendinjue <mugendinjue7@gmail.com>
2019-10-19 23:05:21 +0300

    add a registration url that points to register view function

--- award/urls.py
+++ award/urls.py
@@ -3,5 +3,6 @@ from . import views as main_views
 
 
 urlpatterns = [
-  path('',main_views.home,name='home')
+  path('',main_views.home,name='home'),
+  path('register/',main_views.register,name='register'),
 ]
