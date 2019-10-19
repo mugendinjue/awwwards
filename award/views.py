0cc5baac77496c34235563667b32fe27c8657ecd
mugendinjue <mugendinjue7@gmail.com>
2019-10-20 00:10:39 +0300

    add a loginrequired decorator to the home view function

--- award/views.py
+++ award/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
 
 
-
-def home(request):
-  return render(request,'main/home.html')
 
 def register(request):
-  reg_form = RegistrationForm()
+  if request.method == 'POST':
+    reg_form = RegistrationForm(request.POST)
+    if reg_form.is_valid():
+      reg_form.save()
+      username = reg_form.cleaned_data.get('username')
+      message = f'Account for {username} has been created successfully.You can proceed to login'
+      return redirect('login')
+  else:
+    reg_form=RegistrationForm()
+      
   return render(request,'auth/registration.html',{'reg_form':reg_form})
+
+
+@login_required
+def home(request):
+  return render(request,'main/home.html')
 
