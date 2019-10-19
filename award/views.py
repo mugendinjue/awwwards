ace8c19b14e5e4e0c58e81cc10aff4108c565387
mugendinjue <mugendinjue7@gmail.com>
2019-10-19 23:05:57 +0300

    add a register view function

--- award/views.py
+++ award/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
 
 
 def home(request):
   return render(request,'main/home.html')
+
+def register(request):
+  reg_form = RegistrationForm()
+  return render(request,'auth/registration.html',{'reg_form':reg_form})
 
