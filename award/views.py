91009fb34ad692d687e118e92c1bfde570660719
mugendinjue <mugendinjue7@gmail.com>
2019-10-20 17:29:01 +0300

    add a profile view function

--- award/views.py
+++ award/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-from .forms import RegistrationForm
+from .forms import RegistrationForm,PostProjectsForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .models import Project,Profile
 
 
 
@@ -22,11 +23,46 @@ def register(request):
 @login_required
 def home(request):
   reg_form=RegistrationForm()
-  return render(request,'main/home.html',{'reg_form':reg_form})
+  projects = Project.query_all()
+  context = {
+    'reg_form':reg_form,
+    'projects':projects
+  }
+  return render(request,'main/home.html',context)
 
 
 @login_required
 def submit(request):
   reg_form=RegistrationForm()
-  return render(request,'main/submit.html',{'reg_form':reg_form})
+ 
+  context = {
+    'reg_form':reg_form,
+  }
+  return render(request,'main/submit.html',context)
 
+
+@login_required
+def post_project(request):
+  if request.method == 'POST':
+    post_form = PostProjectsForm(request.POST,request.FILES)
+    if post_form.is_valid():
+      the_post = post_form.save(commit=False)
+      the_post.user = request.user
+      the_post.save()
+      return redirect('home')
+  else:
+    post_form = PostProjectsForm()
+  context = {
+    'post_form':post_form,
+  }
+  return render(request,'main/postproject.html',context)
+
+
+@login_required
+def profile(request):
+  profile = Profile.get_user_profile(request.user.id)
+  projects = Project.objects.filter(user_id = request.user.id ).all()
+  context = {
+    'projects':projects
+  }
+  return render(request,'auth/profile.html',context)
