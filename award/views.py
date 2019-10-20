3b924a34cde43ccd1d17fc8a054cc79ad4e34b6e
mugendinjue <mugendinjue7@gmail.com>
2019-10-20 11:22:34 +0300

    add a submit project view function

--- award/views.py
+++ award/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 
@@ -10,7 +11,7 @@ def register(request):
     if reg_form.is_valid():
       reg_form.save()
       username = reg_form.cleaned_data.get('username')
-      message = f'Account for {username} has been created successfully.You can proceed to login'
+      messages.success(request,f'Account for {username} has been created successfully.You can proceed to login')
       return redirect('login')
   else:
     reg_form=RegistrationForm()
@@ -20,5 +21,12 @@ def register(request):
 
 @login_required
 def home(request):
-  return render(request,'main/home.html')
+  reg_form=RegistrationForm()
+  return render(request,'main/home.html',{'reg_form':reg_form})
+
+
+@login_required
+def submit(request):
+  reg_form=RegistrationForm()
+  return render(request,'main/submit.html',{'reg_form':reg_form})
 
