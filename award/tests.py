c4d2cf601bdb8a97002af6c6fa0238596d3d758e
mugendinjue <mugendinjue7@gmail.com>
2019-10-20 17:27:10 +0300

    add a save method test to the class project test

--- award/tests.py
+++ award/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Project,Tag,Technology
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestProjectModel(TestCase):
+  def setUp(self):
+    self.new_tag = Tag(name = "cars")
+    self.new_tag.save()
+    self.new_tech = Technology(name = "HTML")
+    self.new_tech.save()
+    self.new_user = User(username = "admin")
+    self.new_project = Project(title = "awwards",landing_page = "awards.png",description = "awarding website",live_link = "awards.com",tag = self.new_tag,posted_on = "2019/10/20",catogory = "tech",technologies = self.new_tech,collaborators = "Denis",user = self.new)
+
