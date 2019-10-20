e4e43d32fbb5dd571da1e41812a2ae3ea8272394
mugendinjue <mugendinjue7@gmail.com>
2019-10-20 17:22:39 +0300

    add a post projects form

--- award/forms.py
+++ award/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from .models import Profile,Project
 
 
 class RegistrationForm(UserCreationForm):
@@ -10,4 +10,13 @@ class RegistrationForm(UserCreationForm):
   class Meta:
     model = User
     fields = ['username','email','password1','password2']
+
+class PostProjectsForm(forms.ModelForm):
+  class Meta:
+    model = Project
+    exclude = ['user']
+    widgets = {
+      'tag': forms.CheckboxSelectMultiple(),
+      'technologies':forms.CheckboxSelectMultiple()
+    }
   
