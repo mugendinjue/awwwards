5b9bbe68e190322d499e4804f3cc1253bad1003d
mugendinjue <mugendinjue7@gmail.com>
2019-10-20 11:21:31 +0300

    add a path to the submit view function

--- award/urls.py
+++ award/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views as main_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
   path('',main_views.home,name='home'),
   path('register/',main_views.register,name='register'),
+  path('login/',auth_views.LoginView.as_view(template_name = 'auth/login.html'),name = 'login'),
+  path('logout/',auth_views.LogoutView.as_view(template_name = 'auth/logout.html'),name='logout'),
+  path('submission',main_views.submit,name='submissions')
 ]
+
+if settings.DEBUG:
+  urlpatterns+= static(
+    settings.MEDIA_URL, document_root = settings.MEDIA_ROOT
+  )
