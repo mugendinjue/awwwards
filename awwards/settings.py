d7de7bd4399527f38a825c9651fd23cb20fdf4c1
mugendinjue <mugendinjue7@gmail.com>
2019-10-21 17:37:52 +0300

    add static setting configurations

--- awwards/settings.py
+++ awwards/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'crispy_forms',
     'ckeditor',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -133,13 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_DIRS = ( 
-    os.path.join(BASE_DIR,'static')
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,"static"),
+)
 
 
 MEDIA_URL = '/media/'
